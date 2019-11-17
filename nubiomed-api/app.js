bfc5a46097783a8390072bab69b66d67a40c02c7
angelmc32 <angel.melendez.c@gmail.com>
2019-11-16 21:27:35 -0600

    :wrench: Added packages and changed config files

--- nubiomed-api/app.js
+++ nubiomed-api/app.js
@@ -4,14 +4,16 @@ const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+// cors package to allow cross-origin resource sharing (CORS) between front-end and back-end
+const cors         = require('cors');
+
 
 mongoose
-  .connect('mongodb://localhost/nubiomed-api', {useNewUrlParser: true})
+  .connect('mongodb://localhost/nubiomed-api', {useNewUrlParser: true, useUnifiedTopology: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -30,6 +32,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// cors package config to allow requests from the url's in the origin array
+app.use(
+  cors({
+    origin: ['http://localhost:3001']
+  })
+);
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -40,14 +49,14 @@ app.use(require('node-sass-middleware')({
       
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Nubiomed API by Mel';
 
 
 
